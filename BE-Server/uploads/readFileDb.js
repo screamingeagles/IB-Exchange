6116489bd44bda0ffaa658957bf89800d3de063f
Web Developer <sg.net123@gmail.com>
2023-02-03 12:25:18 +0400

    wokring now

--- BE-Server/uploads/readFileDb.js
+++ BE-Server/uploads/readFileDb.js
@@ -1,7 +1,103 @@
 import fs from 'fs';
 
 export default {
-    storeJsonToFile: async function() {
+
+    getTopicsList: async function () {
+        try {
+            var rs = fs.readFileSync('./uploads/myDb.json', 'utf8');    // run it Syncronusly 
+            var data = JSON.parse(rs);                                  //now it an object                                    
+            return data.Topics;
+        }
+        catch (err) {
+            return err;
+        }
+    },
+
+    getQuestionsList: async function () {
+        try {
+            var rs = fs.readFileSync('./uploads/myDb.json', 'utf8');    // run it Syncronusly 
+            var data = JSON.parse(rs);                                  //now it an object                        
+            return data.Questions;
+        }
+        catch (err) {
+            return err;
+        }
+    },
+
+    getAnswersList: async function () {
+        try {
+            var rs = fs.readFileSync('./uploads/myDb.json', 'utf8');    // run it Syncronusly 
+            var data = JSON.parse(rs);                                  //now it an object            
+            return data.Answers;
+        }
+        catch (err) {
+            return err;
+        }
+    },
+
+    getQuestionByID: async function (param) {
+        try {
+            var rs = fs.readFileSync('./uploads/myDb.json', 'utf8');    // run it Syncronusly 
+            var data = JSON.parse(rs);                                  //now it an object                 
+
+            const result = data.Questions.filter(item => {
+                return item.QID == param;           // first get object meeting the criterea
+            });
+
+            console.log("Here checking default");
+            this.getTopicsList()
+            .then(result => {
+                console.log(result);
+            }).catch(err => console.error(err));
+            /*
+           
+            */
+            //var s = module.default.getTopicByID(result.TID);
+            //result.push(getTopicByID(result.TID));
+            //result.push(getUserByID(result.UID));
+            return result;
+        }
+        catch (err) {
+            console.log(err);
+            return err;
+        }
+    },
+
+
+    getTopicByID: function (param) {
+        try {
+            var rs = fs.readFileSync('./uploads/myDb.json', 'utf8');    // run it Syncronusly 
+            var data = JSON.parse(rs);                                  //now it an object                 
+
+            return data.Topics.filter(item => {
+                return item.TID == param;           // first get object meeting the criterea
+            }).map(function (obj) {
+                return obj.TopicName;               // get name from object
+            });
+        }
+        catch (err) {
+            return err;
+        }
+    },
+
+    getUserByID: async function (param) {
+        try {
+            var rs = fs.readFileSync('./uploads/myDb.json', 'utf8');    // run it Syncronusly 
+            var data = JSON.parse(rs);                                  //now it an object                 
+
+            return data.Users.filter(item => {
+                return item.UID == param;           // first get object meeting the criterea
+            }).map(function (obj) {
+                return obj.UserName;               // get name from object
+            });
+        }
+        catch (err) {
+            return err;
+        }
+    },
+
+
+    storeJsonToFile: async function () {
         var obj = {
             table: [],
             designation: []
@@ -13,7 +109,7 @@ export default {
         var json = JSON.stringify(obj);
         fs.writeFile('./uploads/myDb.json', json, 'utf8', function () { /*console.log("---------- Done Saving Db ------------");*/ });
     },
-    getStaffList: async function() {
+    getStaffList: async function () {
         try {
             var rs = fs.readFileSync('./uploads/myDb.json', 'utf8');    // run it Syncronusly 
             var data = JSON.parse(rs);                          //now it an object            
@@ -23,7 +119,7 @@ export default {
             return err;
         }
     },
-    getStaffDetail: async function(param) {
+    getStaffDetail: async function (param) {
         try {
             var rs = fs.readFileSync('./uploads/myDb.json', 'utf8');    // run it Syncronusly 
             var data = JSON.parse(rs);                          //now it an object            
@@ -35,7 +131,7 @@ export default {
             return err;
         }
     },
-    getDesignationList:async function () {
+    getDesignationList: async function () {
         try {
             var rs = fs.readFileSync('./uploads/myDb.json', 'utf8');    // run it Syncronusly 
             var data = JSON.parse(rs);                          //now it an object            
@@ -47,7 +143,7 @@ export default {
             return err;
         }
     },
-    getMaxStaffID:async function() {
+    getMaxStaffID: async function () {
         try {
             var rs = fs.readFileSync('./uploads/myDb.json', 'utf8');    // run it Syncronusly 
             var data = JSON.parse(rs);
@@ -101,6 +197,17 @@ export default {
         catch (err) {
             return err;
         }
+    },
+    equijoin: function (xs, ys, primary, foreign, sel) {
+        const ix = xs.reduce((ix, row) => // loop through m items
+            ix.set(row[primary], row),    // populate index for primary table
+            new Map);                         // create an index for primary table
+
+        return ys.map(row =>              // loop through n items
+            sel(ix.get(row[foreign]),     // get corresponding row from primary
+                row));                        // select only the columns you need
     }
+
+    //getTopicsList: getTopicsList;
 };
 
